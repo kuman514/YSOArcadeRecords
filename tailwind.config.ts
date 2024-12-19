78f43637f2589ccafb0feac84162733043a880c6
kuman514 <hadjadj0@gmail.com>
2024-12-19 19:41:28 +0900

    chore: add palette to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ export default {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        primary: 'var(--primary-color)',
+        hovering: 'var(--hovering-color)',
+        player1: 'var(--player1-color)',
+        player2: 'var(--player2-color)',
+        error: 'var(--error-color)',
       },
     },
   },

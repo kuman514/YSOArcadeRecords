a6706a2bbfaf08291aac5b7fa7183f8dbd814351
kuman514 <hadjadj0@gmail.com>
2024-12-20 19:41:49 +0900

    style: setup alternative header laytou & add menu icon button

--- src/shared/main-header/alternative.tsx
+++ src/shared/main-header/alternative.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
+
+import StackSvgRepoComSvg from '^/public/icons/stack-svgrepo-com.svg';
 
 import { HEIGHT_AMOUNT } from './constants';
 
@@ -28,8 +31,25 @@ export default function AlternativeHeader() {
   }, []);
 
   return (
-    <div className={`${isShow ? '' : 'hidden'} fixed top-0 left-0`}>
-      Alternative Header Placeholder
+    <div
+      className={`${
+        isShow ? '' : 'hidden'
+      } fixed top-0 left-0 w-full h-16 flex flex-row items-center px-2 gap-2 bg-primary`}
+    >
+      <button
+        className="w-12 h-12 p-2"
+        onClick={() => {
+          /**
+           * @todo
+           * Add showing menu sidebar
+           */
+        }}
+      >
+        <StackSvgRepoComSvg width="100%" height="100%" fill="#ffffff" />
+      </button>
+      <Link href="/" className="text-white">
+        YSOArcadeRecords
+      </Link>
     </div>
   );
 }

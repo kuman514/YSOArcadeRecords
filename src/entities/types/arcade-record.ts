a3359d3f8a30c46d1ab4ab309441bed367549b15
kuman514 <hadjadj0@gmail.com>
2024-12-22 23:50:04 +0900

    docs: append players, player-side, and achieved-date to arcade record type

--- src/entities/types/arcade-record.ts
+++ src/entities/types/arcade-record.ts
@@ -12,11 +12,28 @@ export interface ArcadeRecord {
   evaluation: string;
 
   /**
+   * The number of players of the play.
+   */
+  players: number;
+
+  /**
+   * The played side of the play.
+   * For example, if the player played on Player 2, the value will be `2`.
+   */
+  playerSide: number;
+
+  /**
    * The stage at the end of play.
    * For example, `24` in Galaga Arrangement, `1-4` in Strikers 1945 II, or `EX5` in TGM3 Sakura Mode.
    * It can be `ALL`, `1-ALL`, `2-ALL` if you all-cleared.
    */
   stage: string;
+
+  /**
+   * The date of the record.
+   * For example, if a record is achieved on Dec 24 2024, the value will be `new Date('2024-12-24')`
+   */
+  achievedAt: Date;
 
   /**
    * `Optional`

b3316041cc6d43503071595fe96a682d44541750
kuman514 <hadjadj0@gmail.com>
2024-12-24 17:34:37 +0900

    style: define layout of record list by type id

--- app/records/[typeId]/page.tsx
+++ app/records/[typeId]/page.tsx
@@ -1,3 +1,6 @@
+import { temporaryPostList } from '^/src/entities/post-list-item/temp';
+import ArcadeRecordPostList from '^/src/features/arcade-record-post-list';
+
 interface Props {
   params: {
     typeId: string;
@@ -5,5 +8,10 @@ interface Props {
 }
 
 export default function RecordListByTypeIdPage({ params }: Props) {
-  return <main></main>;
+  return (
+    <main className="w-full h-full max-w-5xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+      <h1 className="text-4xl font-bold">{params.typeId} 기록 목록</h1>
+      <ArcadeRecordPostList arcadeRecordPostListItems={temporaryPostList} />
+    </main>
+  );
 }

b44832d2fc259c1e07bee3228f725b61f23a74f1
kuman514 <hadjadj0@gmail.com>
2024-12-27 19:13:53 +0900

    fix: align overlay close button with alternative-header's menu button

--- src/shared/overlay/index.tsx
+++ src/shared/overlay/index.tsx
@@ -37,7 +37,7 @@ export default function Overlay({ children, overlayContent }: Props) {
             <div className="fixed left-0 top-0 w-screen h-dvh bg-[rgba(0,0,0,0.4)] z-50">
               {overlayContent}
               <button
-                className="fixed left-0 top-0 w-12 h-12 p-2"
+                className="fixed left-0 top-0 w-12 h-12 p-2 m-2"
                 onClick={() => {
                   setIsShowOverlay(false);
                 }}

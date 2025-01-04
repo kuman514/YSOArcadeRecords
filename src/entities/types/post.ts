8e754ce80413bef6dc4093885a8e1f57d89c90a3
kuman514 <hadjadj0@gmail.com>
2025-01-04 14:44:35 +0900

    fix: add title, youtube-url, thumbnail, images to arcade record post

--- src/entities/types/post.ts
+++ src/entities/types/post.ts
@@ -1,8 +1,21 @@
 import { ArcadeRecord } from './arcade-record';
-import { Commentable, HavingAuthor, Taggable } from './post-compositions';
+import {
+  Commentable,
+  HavingAuthor,
+  HavingImages,
+  HavingThumbnail,
+  HavingYouTube,
+  Taggable,
+} from './post-compositions';
 
 export interface BasePost extends HavingAuthor, Commentable, Taggable {
   postId: string;
+  title: string;
 }
 
-export interface ArcadeRecordPost extends BasePost, ArcadeRecord {}
+export interface ArcadeRecordPost
+  extends BasePost,
+    ArcadeRecord,
+    HavingThumbnail,
+    HavingImages,
+    HavingYouTube {}

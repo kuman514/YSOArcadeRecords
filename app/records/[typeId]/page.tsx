90736885db3d902a9671028342071bc4665b0e5e
kuman514 <hadjadj0@gmail.com>
2025-01-06 22:55:58 +0900

    improvement: shrink record list page width for better visibility

--- app/records/[typeId]/page.tsx
+++ app/records/[typeId]/page.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function RecordListByTypeIdPage({ params }: Props) {
   return (
-    <main className="w-full h-full max-w-5xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+    <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
       <h1 className="text-4xl font-bold">{params.typeId} 기록 목록</h1>
       <ArcadeRecordPostList arcadeRecordPostListItems={temporaryPostList} />
     </main>
--- app/records/page.tsx
+++ app/records/page.tsx
@@ -3,7 +3,7 @@ import ArcadeRecordPostList from '^/src/features/arcade-record-post-list';
 
 export default function RecordListPage() {
   return (
-    <main className="w-full h-full max-w-5xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+    <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
       <h1 className="text-4xl font-bold">모든 아케이드 기록 목록</h1>
       <ArcadeRecordPostList arcadeRecordPostListItems={temporaryPostList} />
     </main>

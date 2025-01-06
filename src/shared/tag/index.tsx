28101dd4cf7c2a2293bec84ce3ee069e1ae44f78
kuman514 <hadjadj0@gmail.com>
2025-01-06 12:55:44 +0900

    draft: add tag

---
+++ src/shared/tag/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function Tag({ children }: Props) {
+  return (
+    <span className="font-mono bg-gray-300 text-red-700 text-sm px-1 py-0.5 rounded">
+      {children}
+    </span>
+  );
+}

77a5b4bea2975e4839996bb4c92a0500043675e6
kuman514 <hadjadj0@gmail.com>
2025-01-07 09:18:32 +0900

    draft: add temporary arcade record article

--- app/records/[typeId]/[recordId]/page.tsx
+++ app/records/[typeId]/[recordId]/page.tsx
@@ -1,3 +1,6 @@
+import ArcadeRecordArticle from '^/src/features/arcade-record-article';
+import { temporaryArcadeRecordArticle } from '^/src/features/arcade-record-article/temp';
+
 interface Props {
   params: {
     typeId: string;
@@ -5,6 +8,10 @@ interface Props {
   };
 }
 
-export default function RecordArticlePage({ params }: Props) {
-  return <main></main>;
+export default function RecordArticlePage({}: Props) {
+  return (
+    <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+      <ArcadeRecordArticle post={temporaryArcadeRecordArticle} />
+    </main>
+  );
 }

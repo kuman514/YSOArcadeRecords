02f67861a6740cc2efac9d48b3519ce7aa667c3b
kuman514 <hadjadj0@gmail.com>
2025-01-21 16:30:18 +0900

    fix: adjust main header & nav link since introduction & criteria are unified to home page

--- src/shared/ui/main-header.tsx
+++ src/shared/ui/main-header.tsx
@@ -8,21 +8,24 @@ export default function MainHeader() {
   return (
     <>
       <header
-        className={`w-full h-32 flex flex-row items-end pl-8 pr-12 pb-4 bg-primary`}
+        className={`w-full h-32 flex flex-row items-end pl-6 pr-6 sm:pl-8 sm:pr-12 pb-4 bg-primary`}
       >
         <SidebarCaller />
-        <div className="w-full h-12 flex flex-col items-center justify-end sm:flex-row sm:items-center sm:justify-between">
-          <Link href="/" className="text-white text-2xl">
+        <div className="w-full h-12 flex flex-col items-center justify-end md:flex-row md:items-center md:justify-between">
+          <Link href="/" className="text-white sm:text-2xl">
             YSOArcadeRecords
           </Link>
 
           <nav>
-            <ul className="flex flex-row gap-12">
-              <li>
-                <NavLink href="/introduction">Introduction</NavLink>
-              </li>
+            <ul className="flex flex-row gap-6">
               <li>
                 <NavLink href="/records">Records</NavLink>
+              </li>
+              <li>
+                <NavLink href="/reviews">Reviews</NavLink>
+              </li>
+              <li>
+                <NavLink href="/gallery">Gallery</NavLink>
               </li>
             </ul>
           </nav>

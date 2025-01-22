133103acf9b4879fb55e526ba312259c76707b4c
kumankoishi.py <7iggif@naver.com>
2025-01-22 14:57:34 +0900

    docs: add dictionary

---
+++ src/entities/dictionary/tag.ts
@@ -0,0 +1,22 @@
+export const tagDictionary: Record<string, string> = {
+  // Dictionary about no miss or no bomb
+  'no-bomb': '노봄 ALL',
+  'no-miss-all': '노미스 ALL',
+  'loop1-no-miss': '1주 노미스',
+
+  // Dictionary about score
+  'new-score-record': '점수 갱신',
+  'new-stage-record': '최대 스테이지 갱신',
+  'world-record': '전국 TOP',
+  'shop-record': '점포 TOP',
+
+  // Dictionary about special events
+  'akatronics-5th-anniversary': '아카트로닉스 5주년 기념',
+  'akatronics-6th-anniversary': '아카트로닉스 6주년 기념',
+  ksf4: 'Korea Shmup Festival 4 참여',
+
+  /**
+   * @todo
+   * Add more dictionary
+   */
+};

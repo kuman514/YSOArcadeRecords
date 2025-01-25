5f4d8117228ef17212824b09ad1bc2ed98b4a289
kuman514 <hadjadj0@gmail.com>
2025-01-25 10:26:20 +0900

    fix: redirect to home page on login in signin/signup page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,15 @@
-import SignInForm from '^/src/features/auth/sign-in-form';
+import { redirect } from 'next/navigation';
 
-export default function SignInPage() {
+import SignInForm from '^/src/features/auth/sign-in-form';
+import { verifyAuth } from '^/src/shared/lib/auth';
+
+export default async function SignInPage() {
+  const result = await verifyAuth();
+
+  if (result.user) {
+    return redirect('/');
+  }
+
   return (
     <main className="w-full h-full max-w-3xl flex flex-col items-center px-4 sm:px-8 py-32 gap-8">
       <h1 className="text-4xl font-bold">로그인</h1>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,15 @@
-import SignUpForm from '^/src/features/auth/sign-up-form';
+import { redirect } from 'next/navigation';
 
-export default function SignUpPage() {
+import SignUpForm from '^/src/features/auth/sign-up-form';
+import { verifyAuth } from '^/src/shared/lib/auth';
+
+export default async function SignUpPage() {
+  const result = await verifyAuth();
+
+  if (result.user) {
+    return redirect('/');
+  }
+
   return (
     <main className="w-full h-full max-w-3xl flex flex-col items-center px-4 sm:px-8 py-32 gap-8">
       <h1 className="text-4xl font-bold">회원가입</h1>

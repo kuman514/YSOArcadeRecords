20bd145118ae0fd92e95b0edb0fe6a6a293fd93a
kuman514 <hadjadj0@gmail.com>
2025-01-26 00:40:32 +0900

    draft: add create page routes

---
+++ app/create/records/page.tsx
@@ -0,0 +1,3 @@
+export default function CreateRecordPage() {
+  return <main>현재 준비 중입니다.</main>;
+}
---
+++ app/create/reviews/page.tsx
@@ -0,0 +1,3 @@
+export default function CreateReviewPage() {
+  return <main>현재 준비 중입니다.</main>;
+}

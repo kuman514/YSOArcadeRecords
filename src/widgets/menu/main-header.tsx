cabf7832b63dcb58970e0344e8164c6b151bc467
kuman514 <hadjadj0@gmail.com>
2025-01-27 11:41:54 +0900

    improvement: apply auth-link to main header & adjust layout of main header

--- src/widgets/menu/main-header.tsx
+++ src/widgets/menu/main-header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import NavLink from '^/src/shared/ui/nav-link';
 
 import AlternativeHeader from './alternative-header';
+import AuthLink from './auth-link';
 import SidebarCaller from './sidebar-caller';
 
 export default function MainHeader() {
@@ -12,24 +13,27 @@ export default function MainHeader() {
         className={`w-full h-32 flex flex-row items-end pl-6 pr-6 sm:pl-8 sm:pr-12 pb-4 bg-primary`}
       >
         <SidebarCaller />
-        <div className="w-full h-12 flex flex-col items-center justify-end md:flex-row md:items-center md:justify-between">
-          <Link href="/" className="text-white sm:text-2xl">
-            YSOArcadeRecords
-          </Link>
+        <div className="w-full flex flex-col">
+          <AuthLink />
+          <div className="w-full h-12 flex flex-col items-center justify-end sm:flex-row sm:items-center sm:justify-between">
+            <Link href="/" className="text-white md:text-2xl">
+              YSOArcadeRecords
+            </Link>
 
-          <nav>
-            <ul className="flex flex-row gap-6">
-              <li>
-                <NavLink href="/records">Records</NavLink>
-              </li>
-              <li>
-                <NavLink href="/reviews">Reviews</NavLink>
-              </li>
-              <li>
-                <NavLink href="/gallery">Gallery</NavLink>
-              </li>
-            </ul>
-          </nav>
+            <nav>
+              <ul className="flex flex-row gap-6">
+                <li>
+                  <NavLink href="/records">Records</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/reviews">Reviews</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/gallery">Gallery</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </header>
       <AlternativeHeader />

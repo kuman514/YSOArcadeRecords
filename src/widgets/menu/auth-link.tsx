559faceeea3030cac080ea275b3c8b6c4e4d8b96
kuman514 <hadjadj0@gmail.com>
2025-01-27 11:41:17 +0900

    draft: add auth-link for main-header

---
+++ src/widgets/menu/auth-link.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+import { signOut } from '^/src/features/auth/sign-out';
+import { verifyAuth } from '^/src/shared/lib/auth';
+
+export default async function AuthLink() {
+  const result = await verifyAuth();
+
+  const renderLinkArea = result.user ? (
+    <>
+      <Link href="/create/records">새기록</Link>
+      <Link href="/create/reviews">새리뷰</Link>
+      <Link href="/create/gallery">새갤러리</Link>
+      <form action={signOut}>
+        <button type="submit">로그아웃</button>
+      </form>
+    </>
+  ) : (
+    <Link href="/signin">관리자 로그인</Link>
+  );
+
+  return (
+    <div className="w-full h-full flex flex-row justify-center sm:justify-end items-center text-white gap-4 text-sm">
+      {renderLinkArea}
+    </div>
+  );
+}

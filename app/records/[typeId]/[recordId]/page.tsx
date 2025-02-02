6d4d871f07086b4c73b314ef40e79c1b7711f0fd
kuman514 <hadjadj0@gmail.com>
2025-02-02 15:29:12 +0900

    draft: add modify record button to record article

--- app/records/[typeId]/[recordId]/page.tsx
+++ app/records/[typeId]/[recordId]/page.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import ArcadeRecordArticle from '^/src/features/arcade-record-article';
 import { getArcadeRecordPostArticle } from '^/src/features/arcade-record-article/data';
 import { convertArcadeRecordPostDBColumnToItems } from '^/src/features/arcade-record-post-list/util';
+import { verifyAuth } from '^/src/shared/lib/auth';
 
 interface Props {
   params: {
@@ -11,10 +13,11 @@ interface Props {
   };
 }
 
-export default function RecordArticlePage({
+export default async function RecordArticlePage({
   params: { typeId, recordId },
 }: Props) {
   const data = getArcadeRecordPostArticle(typeId, recordId);
+  const user = await verifyAuth();
 
   if (!data) {
     notFound();
@@ -22,8 +25,15 @@ export default function RecordArticlePage({
 
   const convertedData = convertArcadeRecordPostDBColumnToItems(data);
 
+  const renderModifyButton = user.user ? (
+    <p className="w-full flex flex-row justify-end items-center">
+      <Link href={`/records/${typeId}/${recordId}/modify`}>수정하기</Link>
+    </p>
+  ) : null;
+
   return (
     <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+      {renderModifyButton}
       <ArcadeRecordArticle post={convertedData} />
     </main>
   );

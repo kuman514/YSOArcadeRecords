ae0a95d962af54b00b79fdcca3fc8c1e47edc4c4
kuman514 <hadjadj0@gmail.com>
2025-02-02 12:30:25 +0900

    chore: expand server action limit to 100mb to save posts with images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
     turbo: {
       rules: {
         '*.svg': {

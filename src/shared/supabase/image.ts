6dbb4b5331c4217bb25a8a2322fe092acbe4e4b1
kuman514 <hadjadj0@gmail.com>
2025-02-16 16:08:42 +0900

    draft: add uploading images to supabase bucket

---
+++ src/shared/supabase/image.ts
@@ -0,0 +1,27 @@
+import { createServerSideClient } from './server';
+
+export async function saveImage(
+  file: File,
+  fileName: string,
+  directory: string
+) {
+  const extension = file.name.split('.').pop();
+  const finalFileName = `${fileName}.${extension}`;
+
+  const supabase = await createServerSideClient();
+  const { data: result, error } = await supabase.storage
+    .from(process.env.NEXT_PUBLIC_SUPABASE_UPLOAD_BUCKET_ID!)
+    .upload(`${directory}/${finalFileName}`, file);
+
+  if (error || !result) {
+    throw new Error('Failed to save image.');
+  }
+
+  const {
+    data: { publicUrl },
+  } = await supabase.storage
+    .from(process.env.NEXT_PUBLIC_SUPABASE_UPLOAD_BUCKET_ID!)
+    .getPublicUrl(result.path);
+
+  return publicUrl;
+}

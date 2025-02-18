a864ae67f60914cfbcf570b1669e4b68554f666b
kuman514 <hadjadj0@gmail.com>
2025-02-19 08:42:32 +0900

    docs: add todo about arcade info database table

--- app/records/[arcadeId]/page.tsx
+++ app/records/[arcadeId]/page.tsx
@@ -1,3 +1,4 @@
+import { arcadeDictionary } from '^/src/entities/dictionary/arcade';
 import { PostListItemProps } from '^/src/entities/post-list-item/props';
 import ArcadeRecordPostList from '^/src/features/arcade-record-post-list';
 import { getArcadeRecordPostListWithArcadeId } from '^/src/features/arcade-record-post-list/data';
@@ -25,9 +26,17 @@ export default async function RecordListByTypeIdPage({ params }: Props) {
     thumbnailUrl: datum.thumbnailUrl,
   }));
 
+  /**
+   * @todo
+   * Make arcade info table that has arcadeId, arcadeName, and more information as columns
+   * Apply arcadeId in arcade info table as foreign key of arcade record table's arcadeId
+   */
+
   return (
     <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
-      <h1 className="text-4xl font-bold">{arcadeId} 기록 목록</h1>
+      <h1 className="text-4xl font-bold">
+        {arcadeDictionary[arcadeId] ?? arcadeId} 기록 목록
+      </h1>
       <ArcadeRecordPostList arcadeRecordPostListItems={postListItems} />
     </main>
   );

7a7f978808c02265835b796f956232a4f299bbeb
kumankoishi.py <7iggif@naver.com>
2025-02-21 22:11:23 +0900

    improvement: add not found for record article not found

---
+++ app/records/[arcadeId]/[arcadeRecordId]/not-found.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+import NotFoundPng from '^/public/status/not-found.png';
+
+export default function ArcadeRecordArticleNotFoundPage() {
+  return (
+    <main className="w-full h-full max-w-3xl flex flex-col items-center px-4 sm:px-8 py-32 gap-8">
+      <div className="w-full h-40 relative">
+        <Image
+          src={NotFoundPng}
+          fill
+          alt="관련 데이터를 찾을 수 없음"
+          className="object-contain"
+        />
+      </div>
+      <h1 className="text-4xl font-bold">기록이 없습니다.</h1>
+    </main>
+  );
+}

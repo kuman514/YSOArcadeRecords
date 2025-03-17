068c11e7adc3006e2256755b43fcda4f760378c5
kumankoishi.py <7iggif@naver.com>
2025-03-18 04:21:29 +0900

    refactor: remove arcade record action state

--- app/api/records/[arcadeRecordId]/route.ts
+++ app/api/records/[arcadeRecordId]/route.ts
@@ -2,10 +2,10 @@ import { revalidatePath } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 import { ArcadeRecordPostDBInput } from '^/src/entities/types/post';
-import { ArcadeRecordActionState } from '^/src/features/arcade-record-article/types';
 import { updateData } from '^/src/shared/supabase/database';
 import { createServerSideClient } from '^/src/shared/supabase/server';
 import { ConditionType } from '^/src/shared/supabase/types';
+import { parseEvaluation } from '^/src/shared/util/parse-evaluation';
 
 export async function PUT(
   request: Request,
@@ -44,51 +44,36 @@ export async function PUT(
     return NextResponse.json(
       {
         result: 'failed',
-        error: 'Requires authentication.',
+        error: '로그인이 필요합니다.',
       },
       { status: 401 }
     );
   }
 
-  const errors: ArcadeRecordActionState['errors'] = {};
+  const isEvaluationVerified = (() => {
+    try {
+      parseEvaluation(evaluation ?? '');
+      return true;
+    } catch {
+      return false;
+    }
+  })();
 
-  if (!title || title.trim().length === 0) {
-    errors.title = '제목을 입력해주십시오.';
-  }
-
-  if (!arcadeId) {
-    errors.arcadeId = '아케이드 부문을 입력해주십시오.';
-  }
-
-  if (!methodId) {
-    errors.methodId = '수단을 입력해주십시오.';
-  }
-
-  if (!achievedAt) {
-    errors.achievedAt = '달성일자를 입력해주십시오.';
-  }
-
-  if (!players) {
-    errors.players = '몇 명이서 플레이했는지 선택해주십시오.';
-  }
-
-  if (!playerSide) {
-    errors.playerSide = '어느 사이드에서 플레이했는지 선택해주십시오.';
-  }
-
-  if (!evaluation) {
-    errors.evaluation = '점수 또는 클리어 타임을 입력해주십시오.';
-  }
-
-  if (!stage) {
-    errors.stage = '어느 스테이지에서 끝났는지 입력해주십시오.';
-  }
-
-  if (!comment) {
-    errors.comment = '코멘터리를 입력해주십시오.';
-  }
-
-  if (Object.keys(errors).length > 0) {
+  if (
+    !arcadeRecordId ||
+    !title ||
+    !arcadeId ||
+    !methodId ||
+    !achievedAt ||
+    !players ||
+    !playerSide ||
+    !evaluation ||
+    !isEvaluationVerified ||
+    !stage ||
+    !comment ||
+    (!thumbnailUrl && !presentThumbnailUrl) ||
+    (presentImageUrls.length === 0 && originalImageUrls.length === 0)
+  ) {
     return NextResponse.json(
       {
         result: 'failed',
@@ -107,15 +92,15 @@ export async function PUT(
     await updateData<Partial<ArcadeRecordPostDBInput>>({
       update: 'records',
       set: {
-        title: title!,
-        arcade_id: arcadeId!,
-        method_id: methodId!,
+        title: title,
+        arcade_id: arcadeId,
+        method_id: methodId,
         players: Number(players),
         player_side: Number(playerSide),
-        evaluation: evaluation!,
-        stage: stage!,
+        evaluation: evaluation,
+        stage: stage,
         rank,
-        comment: comment!,
+        comment: comment,
         tags:
           tags
             ?.split(',')
@@ -125,7 +110,7 @@ export async function PUT(
         youtube_id: youTubeId,
         thumbnail_url: thumbnailUrl ?? presentThumbnailUrl,
         image_urls: presentImageUrls.concat(originalImageUrls),
-        achieved_at: achievedAt!,
+        achieved_at: achievedAt,
         modified_at: formattedDate,
       },
       where: [
@@ -143,9 +128,9 @@ export async function PUT(
     return NextResponse.json(
       {
         result: 'failed',
-        error: '아케이드 기록 수정 실패.',
+        error: '아케이드 기록 수정 실패. 다시 시도하여 주십시오.',
       },
-      { status: 400 }
+      { status: 500 }
     );
   }
 }

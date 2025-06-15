71cd157ff606f81378ec9d20306d5003405636d6
kuman514 <hadjadj0@gmail.com>
2025-06-15 14:26:49 +0900

    refactor: move use-state, use-effect, total-link-trees to link-tree component

--- src/features/sidebar/link-tree.tsx
+++ src/features/sidebar/link-tree.tsx
@@ -1,16 +1,52 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { useModalStore } from '^/src/shared/modal/store';
 import { ModalType } from '^/src/shared/modal/types';
 import LinkTree from '^/src/shared/ui/link-tree';
 import { LinkTreeNode } from '^/src/shared/ui/types';
 
-interface Props {
-  linkTrees: LinkTreeNode[];
-}
+import { getArcadeRecordTypeCount } from './data';
 
-export default function SidebarLinkTree({ linkTrees }: Props) {
+export default function SidebarLinkTree() {
   const setModal = useModalStore((state) => state.setModal);
+
+  const [arcadeRecordTypeCount, setArcadeRecordTypeCount] = useState<
+    LinkTreeNode[]
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getArcadeRecordTypeCount();
+      setArcadeRecordTypeCount(
+        data.map((item) => ({
+          href: `/records/${item.arcadeId}`,
+          label: `${item.arcadeId} (${item.length})`,
+        }))
+      );
+    })();
+  }, []);
+
+  const totalLinkTrees: LinkTreeNode[] = [
+    {
+      href: '/',
+      label: 'YSOArcadeRecords',
+    },
+    {
+      href: '/records',
+      label: '아케이드 게임 기록',
+      children: arcadeRecordTypeCount,
+    },
+    {
+      href: '/reviews',
+      label: '아케이드 관련 리뷰',
+    },
+    {
+      href: '/gallery',
+      label: '아케이드 관련 갤러리',
+    },
+  ];
 
   return (
     <div
@@ -26,7 +62,7 @@ export default function SidebarLinkTree({ linkTrees }: Props) {
         }
       }}
     >
-      {linkTrees.map((linkTree, index) => (
+      {totalLinkTrees.map((linkTree, index) => (
         <LinkTree key={`link-tree-${index}`} node={linkTree} />
       ))}
     </div>

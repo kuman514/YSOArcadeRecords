508d634509a920383c75f4cadd944690a92e9d50
kuman514 <hadjadj0@gmail.com>
2025-06-15 14:24:28 +0900

    fix: apply getting arcade record type count to link tree

--- src/features/sidebar/index.tsx
+++ src/features/sidebar/index.tsx
@@ -1,13 +1,53 @@
 'use client';
 
-import { LINK_TREES } from './constants';
+import { useEffect, useState } from 'react';
+
+import { LinkTreeNode } from '^/src/shared/ui/types';
+
+import { getArcadeRecordTypeCount } from './data';
 import SidebarLinkTree from './link-tree';
 
 export default function Sidebar() {
+  const [arcadeRecordTypeCount, setArcadeRecordTypeCount] = useState<
+    LinkTreeNode[]
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getArcadeRecordTypeCount();
+      setArcadeRecordTypeCount(
+        data.map((item) => ({
+          href: `/records/${item.arcadeId}`,
+          label: `${item.arcadeId} (${item.length})`,
+        }))
+      );
+    })();
+  }, []);
+
+  const arcadeRecordTypeLinkTree: LinkTreeNode[] = [
+    {
+      href: '/',
+      label: 'YSOArcadeRecords',
+    },
+    {
+      href: '/records',
+      label: '아케이드 게임 기록',
+      children: arcadeRecordTypeCount,
+    },
+    {
+      href: '/reviews',
+      label: '아케이드 관련 리뷰',
+    },
+    {
+      href: '/gallery',
+      label: '아케이드 관련 갤러리',
+    },
+  ];
+
   return (
     <div className="w-full h-full bg-primary text-white max-w-[40rem] grid grid-rows-[4rem_1fr_4rem]">
       <div />
-      <SidebarLinkTree linkTrees={LINK_TREES} />
+      <SidebarLinkTree linkTrees={arcadeRecordTypeLinkTree} />
       <div className="w-full h-full flex justify-center items-center">
         YSO as kuman514
       </div>

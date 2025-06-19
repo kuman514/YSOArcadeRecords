aa415d67b6cecbceb871d261313b2581d0b74558
kuman514 <hadjadj0@gmail.com>
2025-06-20 02:21:34 +0900

    fix: add empty fallback for recent posts

--- src/widgets/recent-post-widget/arcade-records.tsx
+++ src/widgets/recent-post-widget/arcade-records.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+
+import EmptyPng from '^/public/status/empty.png';
 import PostListItem from '^/src/entities/post-list-item';
 import { getArcadeRecordPostList } from '^/src/features/arcade-record-article/arcade-record-post-list/data';
 
@@ -17,7 +20,7 @@ export default async function RecentArcadeRecordPostsWidget() {
     thumbnailUrl: datum.thumbnailUrl,
   }));
 
-  return (
+  return arcadeRecordPosts.length > 0 ? (
     <ul className="w-full flex flex-col gap-4">
       {arcadeRecordPosts.map((postListItem, index) => (
         <PostListItem
@@ -26,5 +29,19 @@ export default async function RecentArcadeRecordPostsWidget() {
         />
       ))}
     </ul>
+  ) : (
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="w-full h-20 relative">
+        <Image
+          src={EmptyPng}
+          fill
+          alt="관련 데이터를 찾을 수 없음"
+          className="object-contain"
+          priority
+          sizes="37.5rem"
+        />
+      </div>
+      <span className="text-xl font-bold">기록이 없습니다.</span>
+    </div>
   );
 }
--- src/widgets/recent-post-widget/reviews.tsx
+++ src/widgets/recent-post-widget/reviews.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+
+import EmptyPng from '^/public/status/empty.png';
 import PostListItem from '^/src/entities/post-list-item';
 import { getReviewPostList } from '^/src/features/review-article/review-post-list/data';
 
@@ -17,7 +20,7 @@ export default async function RecentReviewPostsWidget() {
     thumbnailUrl: datum.thumbnailUrl,
   }));
 
-  return (
+  return reviewPosts.length > 0 ? (
     <ul className="w-full flex flex-col gap-4">
       {reviewPosts.map((postListItem, index) => (
         <PostListItem
@@ -26,5 +29,19 @@ export default async function RecentReviewPostsWidget() {
         />
       ))}
     </ul>
+  ) : (
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="w-full h-20 relative">
+        <Image
+          src={EmptyPng}
+          fill
+          alt="관련 데이터를 찾을 수 없음"
+          className="object-contain"
+          priority
+          sizes="37.5rem"
+        />
+      </div>
+      <span className="text-xl font-bold">리뷰가 없습니다.</span>
+    </div>
   );
 }

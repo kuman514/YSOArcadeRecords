5382094a97ecbcd6e4af524936bdfafb30811492
kuman514 <hadjadj0@gmail.com>
2025-07-28 07:47:24 +0900

    test: add sample test

---
+++ index.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Sample test', () => {
+  it('should say that 1 + 2 === 3', () => {
+    expect(1 + 2).toStrictEqual(3);
+  });
+
+  // prettier-ignore
+  it('should say that 1 + 2 !== \'3\'', () => {
+    expect(1 + 2).not.toStrictEqual('3');
+  });
+});

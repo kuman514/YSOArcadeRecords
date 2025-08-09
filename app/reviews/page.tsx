3e09796a3ee0dbf19a1e15c620ad896f48dce29a
kuman514 <hadjadj0@gmail.com>
2025-08-09 09:26:25 +0900

    refactor: add & apply converting review-post to post-list-item

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -5,6 +5,7 @@ import { ITEMS_PER_PAGE } from '^/src/entities/constants/pagenation';
 import { PostListItemProps } from '^/src/entities/post-list-item/props';
 import ReviewPostList from '^/src/features/review-article/review-post-list';
 import { getReviewPostList } from '^/src/features/review-article/review-post-list/data';
+import { convertReviewPostToPostListItem } from '^/src/features/review-article/review-post-list/util';
 
 export default async function ReviewListPage() {
   const data = await getReviewPostList({
@@ -12,15 +13,9 @@ export default async function ReviewListPage() {
     to: ITEMS_PER_PAGE - 1,
   });
 
-  const postListItems: PostListItemProps[] = data.map((datum) => ({
-    title: datum.title,
-    memo: `${datum.subjectType} - ${datum.subjectName}`,
-    dateToDisplay: datum.createdAt,
-    tags: datum.tags,
-    isHaveYouTube: Boolean(datum.youTubeId),
-    href: `/reviews/${datum.reviewId}`,
-    thumbnailUrl: datum.thumbnailUrl,
-  }));
+  const postListItems: PostListItemProps[] = data.map(
+    convertReviewPostToPostListItem
+  );
 
   return (
     <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">

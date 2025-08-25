863192a6240e71671d510319df5253cdb4d875cb
kuman514 <hadjadj0@gmail.com>
2025-08-26 04:45:18 +0900

    draft: define create gallery page

--- app/create/gallery/page.tsx
+++ app/create/gallery/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
 
-import PreparingNotice from '^/src/shared/notice/preparing';
 import { createServerSideClient } from '^/src/shared/supabase/server';
 
 export default async function CreateGalleryPage() {
@@ -11,5 +10,13 @@ export default async function CreateGalleryPage() {
     redirect('/');
   }
 
-  return <PreparingNotice />;
+  return (
+    <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+      <h1 className="text-4xl font-bold">새 갤러리 사진</h1>
+      {/**
+       * @todo
+       * Add gallery form here
+       */}
+    </main>
+  );
 }

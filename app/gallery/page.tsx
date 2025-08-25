00c0e8382e165423a9a28d6124d64935e8bffb84
kuman514 <hadjadj0@gmail.com>
2025-08-25 21:04:34 +0900

    draft: accept props for gallery-list-element

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,8 @@
-import GalleryListElement from '^/src/features/gallery/gallery-list/element';
-
 export default function GalleryListPage() {
   return (
     <main className="w-full h-full max-w-3xl px-4 sm:px-8 py-32">
       <div className="w-full h-full flex flex-row flex-wrap items-start">
+        {/* <GalleryListElement />
         <GalleryListElement />
         <GalleryListElement />
         <GalleryListElement />
@@ -12,8 +11,7 @@ export default function GalleryListPage() {
         <GalleryListElement />
         <GalleryListElement />
         <GalleryListElement />
-        <GalleryListElement />
-        <GalleryListElement />
+        <GalleryListElement /> */}
       </div>
     </main>
   );
--- src/features/gallery/gallery-list/element.tsx
+++ src/features/gallery/gallery-list/element.tsx
@@ -1,36 +1,24 @@
 import Image from 'next/image';
-
 import Link from 'next/link';
 
-// interface Props {
-//   post: GalleryPost;
-// }
+import { GalleryPost } from '^/src/entities/types/post';
 
-export default function GalleryListElement(/* { post }: Props */) {
-  // return (
-  //   <li className="w-1/3 aspect-square p-0.5 overflow-hidden hover:[&_.post-thumbnail]:scale-125 [&_.post-thumbnail]:transition-all hover:[&_.post-thumbnail]:brightness-110">
-  //     <Link href={`/gallery/${post.galleryId}`} className="w-full h-full">
-  //       <Image
-  //         className="w-full h-full post-thumbnail object-cover"
-  //         src={post.thumbnailUrl}
-  //         alt={post.title}
-  //         unoptimized
-  //       />
-  //     </Link>
-  //   </li>
-  // );
+interface Props {
+  post: GalleryPost;
+}
 
+export default function GalleryListElement({ post }: Props) {
   return (
     <Link
-      href="/gallery"
+      href={`/gallery/${post.galleryId}`}
       className="w-1/3 aspect-square p-0.5 hover:[&_.post-thumbnail]:scale-125 [&_.post-thumbnail]:transition-all hover:[&_.post-thumbnail]:brightness-110 relative"
     >
       <div className="w-full h-full overflow-hidden relative">
         <Image
           fill
           className="w-full h-full post-thumbnail object-cover"
-          src="https://pbs.twimg.com/profile_banners/2580505316/1587998528"
-          alt="애기코이시"
+          src={post.thumbnailUrl}
+          alt={post.title}
           unoptimized
         />
       </div>

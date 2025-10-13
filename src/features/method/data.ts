ef3b67e360e74f3342e35c32a581a3c2f0083173
kuman514 <hadjadj0@gmail.com>
2025-10-13 09:15:39 +0900

    draft: define modifying/deleting methods

--- src/features/method/data.ts
+++ src/features/method/data.ts
@@ -1,5 +1,10 @@
-import { MethodDBColumn } from '^/src/entities/types/method';
-import { selectData } from '^/src/shared/supabase/database';
+import { Method, MethodDBColumn } from '^/src/entities/types/method';
+import {
+  deleteData,
+  selectData,
+  updateData,
+} from '^/src/shared/supabase/database';
+import { ConditionType } from '^/src/shared/supabase/types';
 
 import { convertMethodDBColumnToMethod } from './util';
 
@@ -11,4 +16,37 @@ export async function getMethodList() {
   });
 
   return result.map(convertMethodDBColumnToMethod);
+}
+
+export async function modifyMethod(
+  methodId: Method['methodId'],
+  newMethod: Method
+) {
+  await updateData<MethodDBColumn>({
+    update: 'arcade_info',
+    set: {
+      method_id: newMethod.methodId,
+      method_name: newMethod.label,
+    },
+    where: [
+      {
+        type: ConditionType.EQUAL,
+        column: 'method_id',
+        value: methodId,
+      },
+    ],
+  });
+}
+
+export async function deleteMethod(methodId: Method['methodId']) {
+  await deleteData({
+    deleteFrom: 'methods',
+    where: [
+      {
+        type: ConditionType.EQUAL,
+        column: 'method_id',
+        value: methodId,
+      },
+    ],
+  });
 }

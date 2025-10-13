b359f61039de8fb50b1af05cd80d023aa049caab
kuman514 <hadjadj0@gmail.com>
2025-10-13 09:15:24 +0900

    draft: define modifying/deleting arcade-info

--- src/features/arcade-info/data.ts
+++ src/features/arcade-info/data.ts
@@ -2,7 +2,11 @@ import {
   ArcadeInfo,
   ArcadeInfoDBColumn,
 } from '^/src/entities/types/arcade-info';
-import { selectData } from '^/src/shared/supabase/database';
+import {
+  deleteData,
+  selectData,
+  updateData,
+} from '^/src/shared/supabase/database';
 import { ConditionType } from '^/src/shared/supabase/types';
 
 import { convertArcadeInfoDBColumnToArcadeInfo } from './util';
@@ -31,4 +35,37 @@ export async function getArcadeInfo(arcadeId: ArcadeInfo['arcadeId']) {
   });
 
   return result.map(convertArcadeInfoDBColumnToArcadeInfo)[0];
+}
+
+export async function modifyArcadeInfo(
+  arcadeId: ArcadeInfo['arcadeId'],
+  newArcadeInfo: ArcadeInfo
+) {
+  await updateData<ArcadeInfoDBColumn>({
+    update: 'arcade_info',
+    set: {
+      arcade_id: newArcadeInfo.arcadeId,
+      arcade_title: newArcadeInfo.label,
+    },
+    where: [
+      {
+        type: ConditionType.EQUAL,
+        column: 'arcade_id',
+        value: arcadeId,
+      },
+    ],
+  });
+}
+
+export async function deleteArcadeInfo(arcadeId: ArcadeInfo['arcadeId']) {
+  await deleteData({
+    deleteFrom: 'arcade_info',
+    where: [
+      {
+        type: ConditionType.EQUAL,
+        column: 'arcade_id',
+        value: arcadeId,
+      },
+    ],
+  });
 }

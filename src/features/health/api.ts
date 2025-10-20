73d8293b7972da2b2dd032bcc013b44520751f9c
kuman514 <hadjadj0@gmail.com>
2025-10-20 13:20:51 +0900

    draft: append maintenance message

--- src/features/health/api.ts
+++ src/features/health/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { HEALTH_API_ENDPOINT } from '^/src/entities/health/constants';
 import { GetHealthApiResponse } from '^/src/entities/health/types';
 
-export async function getHealth() {
+export async function getHealth(): Promise<GetHealthApiResponse> {
   try {
     const response = await axios.get<GetHealthApiResponse>(
       HEALTH_API_ENDPOINT,
@@ -13,10 +13,12 @@ export async function getHealth() {
         },
       }
     );
-    console.log(response.data);
-    return response.data.status;
-  } catch (error) {
-    console.error(error);
-    return 'closed';
+    return response.data;
+  } catch {
+    return {
+      status: 'closed',
+      maintenanceMessage:
+        '사이트 로딩 중 일시적인 에러가 발생하였습니다. 이후에도 지속적으로 문제 발생 시 아래 카카오톡 오픈채팅방으로 연락 부탁드립니다.',
+    };
   }
 }

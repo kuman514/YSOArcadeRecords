2d0db92fb1fa16bd92b088ead28f121ac63a0b12
kumankoishi.py <7iggif@naver.com>
2025-11-21 05:53:21 +0900

    improvement: add & apply button-movable review detail input

--- src/shared/ui/multiple-text-form-input.tsx
+++ src/shared/ui/multiple-text-form-input.tsx
@@ -7,6 +7,7 @@ interface Props {
   mainLabel: string;
   appendButtonLabel: string;
   onChange: (index: number, newValue: string) => void;
+  onSwap: (index: number, targetIndex: number) => void;
   onAppend: () => void;
   onDelete: (index: number) => void;
 }
@@ -17,6 +18,7 @@ export default function MultipleTextFormInput({
   mainLabel,
   appendButtonLabel,
   onChange,
+  onSwap,
   onAppend,
   onDelete,
 }: Props) {
@@ -24,7 +26,7 @@ export default function MultipleTextFormInput({
     <p className="w-full flex flex-col gap-2">
       <label htmlFor={name}>{mainLabel}</label>
       {values.map((value, index) => (
-        <span key={value.id} className="flex flex-row gap-2">
+        <span key={value.tmpId} className="flex flex-row gap-2">
           <FormTextArea
             type="text"
             id={name}
@@ -34,15 +36,37 @@ export default function MultipleTextFormInput({
               onChange(index, event.currentTarget.value);
             }}
           />
-          <button
-            type="button"
-            onClick={() => {
-              onDelete(index);
-            }}
-            className="cursor-pointer"
-          >
-            X
-          </button>
+          <span className="flex flex-col gap-4 justify-center items-center">
+            <button
+              type="button"
+              onClick={() => {
+                onSwap(index, Math.max(index - 1, 0));
+              }}
+              disabled={index === 0}
+              className="cursor-pointer disabled:cursor-auto disabled:opacity-0"
+            >
+              ⬆️
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                onDelete(index);
+              }}
+              className="cursor-pointer"
+            >
+              ❌
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                onSwap(index, Math.min(index + 1, values.length - 1));
+              }}
+              disabled={index === values.length - 1}
+              className="cursor-pointer disabled:cursor-auto disabled:opacity-0"
+            >
+              ⬇️
+            </button>
+          </span>
         </span>
       ))}
       <button

dce419b65f2a1d6379ab12f3045251ba0c7c1c9d
kumankoishi <hadjadj0@gmail.com>
2025-12-02 00:58:23 +0900

    improvement: add toggling gadgets of image-zoom-controllers in records, reviews, gallery (#125)
    * draft: add on-click image area for image-zoom-controller
    
    * improvement: apply to image-viewer for records & reviews
    
    * improvement: add & apply gallery post viewer
    
    * refactor: move partial render to local variable

--- app/gallery/[galleryId]/page.tsx
+++ app/gallery/[galleryId]/page.tsx
@@ -3,10 +3,7 @@ import { notFound } from 'next/navigation';
 
 import CloseSvgRepoComSvg from '^/public/icons/close-svgrepo-com.svg';
 import { getGallery } from '^/src/features/gallery/data';
-import DeleteGalleryForm from '^/src/features/gallery/delete-form';
-import ImageZoomController from '^/src/shared/image-zoom-controller';
-import { CopyLinkButton } from '^/src/shared/share/copy-link';
-import { ShareToTwitterButton } from '^/src/shared/share/share-to-twitter';
+import GalleryPostViewer from '^/src/features/gallery/viewer';
 import { createServerSideClient } from '^/src/shared/supabase/server';
 
 interface Props {
@@ -26,37 +23,13 @@ export default async function GalleryPostPage({ params }: Props) {
     notFound();
   }
 
-  const renderModifyButton = !(error || !data?.user) ? (
-    <div className="w-full flex flex-row justify-end gap-2 pointer-events-auto">
-      <Link
-        className="px-4 py-2 bg-primary hover:bg-hovering text-white rounded-sm cursor-pointer"
-        href={`/gallery/${galleryId}/modify`}
-      >
-        수정하기
-      </Link>
-      <DeleteGalleryForm galleryId={galleryId} />
-    </div>
-  ) : null;
-
   return (
     <main className="w-full h-full max-w-3xl px-4 sm:px-8 py-32">
       <div className="fixed left-0 top-0 w-screen h-dvh bg-[rgba(0,0,0)] z-50 touch-none">
-        <ImageZoomController
-          imageUrl={galleryPost.imageUrl}
-          alt={galleryPost.title}
+        <GalleryPostViewer
+          galleryPost={galleryPost}
+          isAuthenticated={Boolean(!(error || !data?.user))}
         />
-
-        <div className="absolute left-0 top-0 w-full h-full flex flex-col justify-between items-center pointer-events-none py-6">
-          <div className="text-white px-4 py-2 bg-[rgba(32,32,32,0.6)] mt-12 sm:mt-0 rounded-lg">
-            주제: {galleryPost.theme.galleryThemeTitle}
-          </div>
-          <div className="w-full flex px-4 flex-col gap-2 justify-center items-center">
-            <div className="text-white px-4 py-2 bg-[rgba(32,32,32,0.6)] rounded-lg">
-              {galleryPost.title}
-            </div>
-            {renderModifyButton}
-          </div>
-        </div>
 
         <Link
           className="fixed left-0 top-0 w-12 h-12 p-2 m-2 cursor-pointer"
@@ -64,14 +37,6 @@ export default async function GalleryPostPage({ params }: Props) {
         >
           <CloseSvgRepoComSvg width="100%" height="100%" />
         </Link>
-
-        <div className="fixed flex flex-row gap-2 right-0 top-0 p-2 m-2">
-          <ShareToTwitterButton
-            postTitle={galleryPost.title}
-            additionalClassName="fill-white stroke-white border-white"
-          />
-          <CopyLinkButton additionalClassName="fill-white stroke-white border-white" />
-        </div>
       </div>
     </main>
   );

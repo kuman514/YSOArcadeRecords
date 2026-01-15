dd2e6651be9725a5b9c6ddf5b0e08d990db3b702
kuman514 <hadjadj0@gmail.com>
2026-01-16 01:34:39 +0900

    fix: add footer padding for contact-button to prevent collision with footer

--- src/features/contact/index.tsx
+++ src/features/contact/index.tsx
@@ -2,7 +2,7 @@ import ContactButton from '^/src/shared/contact';
 
 export default function Contact() {
   return (
-    <div className="fixed left-0 top-0 px-8 py-4 w-screen h-dvh flex justify-end items-end z-40 pointer-events-none">
+    <div className="h-20 w-screen">
       <ContactButton />
     </div>
   );

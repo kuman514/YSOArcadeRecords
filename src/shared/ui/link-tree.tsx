2bf9a9d3841fa29a0067ab3d113767b31bb9e5ed
kuman514 <hadjadj0@gmail.com>
2026-02-03 03:37:02 +0900

    improvement: add folding link-tree to prevent long-scrolling sidebar

--- src/shared/ui/link-tree.tsx
+++ src/shared/ui/link-tree.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { LinkTreeNode } from './types';
 
@@ -7,17 +10,34 @@ interface Props {
 }
 
 export default function LinkTree({ node }: Props) {
-  const renderSubnode = node.children ? (
-    <ul className="flex flex-col gap-2">
-      {node.children.map((subnode, index) => (
-        <LinkTree key={`subnode-${index}`} node={subnode} />
-      ))}
-    </ul>
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const renderSubnode =
+    node.children && isOpen ? (
+      <ul className="flex flex-col gap-2">
+        {node.children.map((subnode, index) => (
+          <LinkTree key={`subnode-${index}`} node={subnode} />
+        ))}
+      </ul>
+    ) : null;
+
+  const renderOpenSubnodeButton = node.children ? (
+    <button
+      className="cursor-pointer"
+      onClick={() => {
+        setIsOpen(!isOpen);
+      }}
+    >
+      {isOpen ? '^' : 'v'}
+    </button>
   ) : null;
 
   return (
     <li className="w-full max-w-[18rem] pl-4 flex flex-col gap-2">
-      <Link href={node.href}>{node.label}</Link>
+      <div className="w-full flex flex-row justify-between items-center">
+        <Link href={node.href}>{node.label}</Link>
+        {renderOpenSubnodeButton}
+      </div>
       {renderSubnode}
     </li>
   );

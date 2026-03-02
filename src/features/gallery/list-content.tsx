71c382a39c8887c85a58891a513bceffb354a02e
kuman514 <hadjadj0@gmail.com>
2026-03-02 23:43:47 +0900

    improvement: apply auto append fetch gallery on scroll

--- src/features/gallery/list-content.tsx
+++ src/features/gallery/list-content.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
-import Button from '^/src/shared/ui/button';
+import Container from '^/src/shared/ui/container';
 
 import { getExtendedGalleryList } from './data-client';
 import GalleryElement from './element';
@@ -22,16 +23,31 @@ export default function GalleryPostListContent() {
 
   const isNextPageButtonDisabled = !isHaveNextPage || isFetching;
 
+  useEffect(() => {
+    function handleOnScroll() {
+      const isScrollSufficient =
+        window.innerHeight + window.scrollY + 40 >= document.body.offsetHeight;
+      if (isScrollSufficient && !isNextPageButtonDisabled) {
+        fetchNextPage();
+      }
+    }
+
+    window.addEventListener('scroll', handleOnScroll);
+    return () => {
+      window.removeEventListener('scroll', handleOnScroll);
+    };
+  }, [isNextPageButtonDisabled]);
+
   const nextPageLabel = (() => {
     if (!isHaveNextPage) {
-      return '마지막 페이지';
+      return '모두 불러옴';
     }
 
     if (isFetching) {
       return '불러오는 중';
     }
 
-    return '더보기';
+    return '스크롤하여 더보기';
   })();
 
   const data = rawData?.pages.map((page) => page.content);
@@ -44,15 +60,7 @@ export default function GalleryPostListContent() {
   return (
     <>
       {renderData}
-      <Button
-        type="button"
-        onClick={() => {
-          fetchNextPage();
-        }}
-        disabled={isNextPageButtonDisabled}
-      >
-        {nextPageLabel}
-      </Button>
+      <Container className="w-full text-center">{nextPageLabel}</Container>
     </>
   );
 }

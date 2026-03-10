db5c04ec02acae3652af274deb42a9dcf094310a
kumankoishi.py <7iggif@naver.com>
2026-03-11 07:35:20 +0900

    fix: remove unnecessary w-full to prevent date input unnecessarily exceeding the screen

--- src/shared/ui/form-input.tsx
+++ src/shared/ui/form-input.tsx
@@ -1,13 +1,12 @@
 import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
 
-interface Props
-  extends DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > {
+interface Props extends DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> {
   type: 'text' | 'password' | 'email' | 'phone' | 'date';
 }
 
 export default function FormInput(props: Props) {
-  return <input {...props} className="w-full retro-rounded-2 px-4 py-2" />;
+  return <input {...props} className="retro-rounded-2 px-4 py-2" />;
 }

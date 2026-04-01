325cb2a2198c1476cd383a3bb3bdb40a40a4fd2a
kumankoishi.py <7iggif@naver.com>
2026-04-01 09:09:37 +0900

    fix: set missing height styles

--- src/shared/ui/loading-indicator.tsx
+++ src/shared/ui/loading-indicator.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function LoadingIndicator({ children }: Props) {
   return (
-    <main className="w-full min-h-[calc(100dvh-18rem)] max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+    <main className="w-full h-full min-h-[calc(100dvh-18rem)] max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
       {children}
     </main>
   );

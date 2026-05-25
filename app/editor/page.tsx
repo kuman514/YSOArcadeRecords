673033dbfc410c5ebf8427d31f985c877e8fd2c6
kuman514 <hadjadj0@gmail.com>
2026-05-25 22:48:25 +0900

    fix: make portals for editor look like button

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 import { createServerSideClient } from '^/src/shared/supabase/server';
+import Container from '^/src/shared/ui/container';
 import { APP_NAME } from '^/src/shared/util/is-production';
 
 export const metadata: Metadata = {
@@ -21,9 +22,27 @@ export default async function EditorPage() {
   return (
     <main className="w-full h-full max-w-3xl px-4 sm:px-8 py-32 flex flex-col gap-4">
       <h1 className="text-4xl font-bold">아케이드 정보 편집기</h1>
-      <Link href="/editor/arcade-info">아케이드 부문 편집기</Link>
-      <Link href="/editor/methods">플레이 수단 편집기</Link>
-      <Link href="/editor/gallery-theme">갤러리 주제 편집기</Link>
+      <Link
+        href="/editor/arcade-info"
+        className="hover:[&>section]:bg-hovering"
+      >
+        <Container className="w-full flex flex-col justify-center items-center">
+          아케이드 부문 편집기
+        </Container>
+      </Link>
+      <Link href="/editor/methods" className="hover:[&>section]:bg-hovering">
+        <Container className="w-full flex flex-col justify-center items-center">
+          플레이 수단 편집기
+        </Container>
+      </Link>
+      <Link
+        href="/editor/gallery-theme"
+        className="hover:[&>section]:bg-hovering"
+      >
+        <Container className="w-full flex flex-col justify-center items-center">
+          갤러리 주제 편집기
+        </Container>
+      </Link>
     </main>
   );
 }

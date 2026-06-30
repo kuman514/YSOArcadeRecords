e5a86c35558f0a5d34523f94642a544feebf0ffe
kumankoishi.py <7iggif@naver.com>
2026-07-01 02:44:50 +0900

    draft: add searching arcade records

---
+++ app/search/page.tsx
@@ -0,0 +1,61 @@
+import { Metadata } from 'next';
+
+import EmptySvg from '^/public/status/empty.svg';
+import { ITEMS_PER_PAGE } from '^/src/entities/constants/pagenation';
+import { APP_NAME } from '^/src/shared/util/is-production';
+import { getArcadeRecordPostList } from '^/src/features/arcade-record-article/arcade-record-post-list/data';
+import { convertArcadeRecordPostToPostListItem } from '^/src/features/arcade-record-article/arcade-record-post-list/util';
+import PostListItem from '^/src/entities/post-list-item';
+
+interface Props {
+  searchParams: Promise<{
+    searchText?: string;
+  }>;
+}
+
+export async function generateMetadata({
+  searchParams,
+}: Props): Promise<Metadata> {
+  const { searchText } = await searchParams;
+  return {
+    title: `${searchText} 검색 결과 :: ${APP_NAME}`,
+  };
+}
+
+export default async function SearchPage({ searchParams }: Props) {
+  const { searchText } = await searchParams;
+  const rawData = await getArcadeRecordPostList(
+    {
+      from: 0,
+      to: ITEMS_PER_PAGE,
+    },
+    {
+      searchText,
+    }
+  );
+
+  const data = rawData.map(convertArcadeRecordPostToPostListItem);
+  const renderData = data.map((postListItem) => (
+    <PostListItem key={postListItem.id} {...postListItem} />
+  ));
+
+  return (
+    <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+      <h1 className="text-4xl font-bold">
+        "{searchText}" 아케이드 기록 검색 결과
+      </h1>
+      {data.length > 0 ? (
+        <ul className="w-full flex flex-col gap-4">{renderData}</ul>
+      ) : (
+        <div className="w-full flex flex-col items-center gap-12 sm:gap-16">
+          <div className="w-full flex flex-col items-center pt-12">
+            <EmptySvg width={`${(100 * 5) / 9}%`} />
+          </div>
+          <span className="text-2xl font-bold text-center">
+            검색 결과가 없습니다.
+          </span>
+        </div>
+      )}
+    </main>
+  );
+}
--- src/features/arcade-record-article/arcade-record-post-list/data.ts
+++ src/features/arcade-record-article/arcade-record-post-list/data.ts
@@ -1,7 +1,7 @@
 import { ArcadeInfo } from '^/src/entities/types/arcade-info';
 import { ArcadeRecordPostDBColumn } from '^/src/entities/types/post';
 import { selectData } from '^/src/shared/supabase/database';
-import { ConditionType, SelectRange } from '^/src/shared/supabase/types';
+import { ConditionType, SelectRange, Where } from '^/src/shared/supabase/types';
 
 import { convertArcadeRecordPostDBColumnToArcadeRecordPost } from './util';
 
@@ -12,19 +12,39 @@ export async function getArcadeRecordPostList(
     searchText?: string;
   }
 ) {
+  const where: Where[] = [];
+
+  if (params?.arcadeId) {
+    where.push({
+      type: ConditionType.EQUAL,
+      column: 'arcade_id',
+      value: params.arcadeId,
+    });
+  }
+
+  if (params?.searchText) {
+    where.push({
+      type: ConditionType.OR,
+      wheres: [
+        {
+          type: ConditionType.ILIKE,
+          column: 'title',
+          value: params.searchText,
+        },
+        {
+          type: ConditionType.ILIKE,
+          column: 'comment',
+          value: params.searchText,
+        },
+      ],
+    });
+  }
+
   const result = await selectData<ArcadeRecordPostDBColumn[]>({
     select:
       'id, arcade_record_id, stage, rank, title, evaluation, score, elapsed_time, achieved_at, tags, youtube_id, thumbnail_url, arcade_info (*), methods (*)',
     from: 'records',
-    where: params?.arcadeId
-      ? [
-          {
-            type: ConditionType.EQUAL,
-            column: 'arcade_id',
-            value: params.arcadeId,
-          },
-        ]
-      : [],
+    where,
     order: [
       {
         column: 'achieved_at',

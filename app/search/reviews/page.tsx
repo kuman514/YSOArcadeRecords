fe4b3ee42d7f12e41d4ca44d744daa6082527d6d
kumankoishi.py <7iggif@naver.com>
2026-07-04 01:02:46 +0900

    draft: add detailed searching reviews

---
+++ app/search/reviews/page.tsx
@@ -0,0 +1,75 @@
+import { Metadata } from 'next';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+
+import EmptySvg from '^/public/status/empty.svg';
+import { APP_NAME } from '^/src/shared/util/is-production';
+import { ITEMS_PER_PAGE } from '^/src/entities/constants/pagenation';
+import { getReviewPostList } from '^/src/features/review-article/review-post-list/data';
+import ReviewSearchResultList from '^/src/features/review-article/search-result-list';
+
+interface Props {
+  searchParams: Promise<{
+    searchText?: string;
+  }>;
+}
+
+export async function generateMetadata({
+  searchParams,
+}: Props): Promise<Metadata> {
+  const { searchText } = await searchParams;
+  return {
+    title: `${searchText} 리뷰 검색 결과 :: ${APP_NAME}`,
+  };
+}
+
+export default async function ReviewSearchPage({ searchParams }: Props) {
+  const { searchText } = await searchParams;
+
+  const queryClient = new QueryClient();
+  const queryKey = ['search', 'reviews', searchText];
+
+  // 첫 번째 페이지(0페이지) 데이터 가져오기 - 다음 페이지 확인을 위해 +1개 더 가져옴
+  const firstPageData = await getReviewPostList(
+    {
+      from: 0,
+      to: ITEMS_PER_PAGE,
+    },
+    {
+      searchText,
+    }
+  );
+
+  // 다음 페이지 존재 여부 확인
+  const isHaveNextPage = firstPageData.length > ITEMS_PER_PAGE;
+  const content = isHaveNextPage
+    ? firstPageData.slice(0, ITEMS_PER_PAGE)
+    : firstPageData;
+
+  // prefetchInfiniteQuery로 첫 페이지 prefetch
+  await queryClient.prefetchInfiniteQuery({
+    queryKey,
+    queryFn: async () => ({
+      content,
+      nextPage: isHaveNextPage ? 1 : null,
+    }),
+    initialPageParam: 0,
+  });
+
+  return (
+    <main className="w-full h-full max-w-3xl flex flex-col items-start px-4 sm:px-8 py-32 gap-8">
+      <h1 className="text-4xl font-bold">"{searchText}" 리뷰 검색 결과</h1>
+      {content.length > 0 ? (
+        <ReviewSearchResultList dehydratedState={dehydrate(queryClient)} />
+      ) : (
+        <div className="w-full flex flex-col items-center gap-12 sm:gap-16">
+          <div className="w-full flex flex-col items-center pt-12">
+            <EmptySvg width={`${(100 * 5) / 9}%`} />
+          </div>
+          <span className="text-2xl font-bold text-center">
+            검색 결과가 없습니다.
+          </span>
+        </div>
+      )}
+    </main>
+  );
+}
